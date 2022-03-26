9a48fd1ad7d7514ea69922711ffe4bf2d4eef0ee
smlit30 <97806497+smlit30@users.noreply.github.com>
2022-03-26 18:35:14 -0500

    Add files via upload

--- PyCitySchools.py
+++ PyCitySchools.py
@@ -61,4 +61,99 @@ for name in student_names:
     if len(name.split()) >= 3:
         students_fixed.append(name)
         
-print(len(students_fixed))
+#print(len(students_fixed))
+
+school_data_complete_df = pd.merge(student_data_df, school_data_df, on=["school_name", "school_name"])
+#print(school_data_complete_df.head())
+
+student_count = school_data_complete_df["Student ID"].count()
+print(student_count)
+
+#print(school_data_complete_df["Student ID"].count())
+school_count = school_data_df["school_name"].count()
+school_count_2 = school_data_complete_df["school_name"].unique()
+#print(school_count_2)
+
+total_budget = school_data_df["budget"].sum()
+print(total_budget)
+
+average_reading_score = school_data_complete_df["reading_score"].mean()
+print(average_reading_score)
+
+average_math_score = school_data_complete_df["math_score"].mean()
+print(average_math_score)
+
+passing_math = school_data_complete_df["math_score"] >= 70
+passing_reading = school_data_complete_df["reading_score"] >= 70
+
+passing_math = school_data_complete_df[school_data_complete_df["math_score"] >= 70]
+passing_reading = school_data_complete_df[school_data_complete_df["reading_score"] >= 70]
+
+passing_math_count = passing_math["student_name"].count()
+passing_reading_count = passing_reading["student_name"].count()
+
+print(passing_math_count)
+print(passing_reading_count)
+
+passing_math_percentage = passing_math_count / student_count * 100
+
+# Calculate the percent that passed reading.
+passing_reading_percentage = passing_reading_count / student_count * 100
+
+print(passing_math_percentage)
+print(passing_reading_percentage)
+
+passing_math_reading = school_data_complete_df[(school_data_complete_df["math_score"] >= 70) & (school_data_complete_df["reading_score"] >= 70)]
+
+#print(passing_math_reading.head())
+
+overall_passing_math_reading_count = passing_math_reading["student_name"].count()
+#print(overall_passing_math_reading_count)
+overall_passing_percentage = overall_passing_math_reading_count / student_count * 100
+#print(overall_passing_percentage)
+
+
+print("See below for district summary statistics")
+
+district_summary_df = pd.DataFrame(
+          [{"Total Schools": school_count,
+          "Total Students": student_count,
+          "Total Budget": total_budget,
+          "Average Math Score": average_math_score,
+          "Average Reading Score": average_reading_score,
+          "% Passing Math": passing_math_percentage,
+         "% Passing Reading": passing_reading_percentage,
+        "% Overall Passing": overall_passing_percentage}])
+
+print(district_summary_df)
+
+def passing_math_percent(pass_math_count, student_count):
+    return pass_math_count / float(student_count) * 100.0
+
+# Format the "Total Students" to have the comma for a thousands separator.
+district_summary_df["Total Students"] = district_summary_df["Total Students"].map("{:,}".format)
+
+#print(district_summary_df["Total Students"])
+
+district_summary_df["Total Budget"] = district_summary_df["Total Budget"].map("${:,.2f}".format)
+
+print(district_summary_df["Total Budget"])
+# Format the columns.
+district_summary_df["Average Math Score"] = district_summary_df["Average Math Score"].map("{:.1f}".format)
+
+district_summary_df["Average Reading Score"] = district_summary_df["Average Reading Score"].map("{:.1f}".format)
+
+district_summary_df["% Passing Math"] = district_summary_df["% Passing Math"].map("{:.0f}".format)
+
+district_summary_df["% Passing Reading"] = district_summary_df["% Passing Reading"].map("{:.0f}".format)
+
+district_summary_df["% Overall Passing"] = district_summary_df["% Overall Passing"].map("{:.0f}".format)
+
+print(district_summary_df)
+
+# Reorder the columns in the order you want them to appear.
+new_column_order = ["Total Schools", "Total Students", "Total Budget","Average Math Score", "Average Reading Score", "% Passing Math", "% Passing Reading", "% Overall Passing"]
+
+# Assign district summary df the new column order.
+district_summary_df = district_summary_df[new_column_order]
+print(district_summary_df)
