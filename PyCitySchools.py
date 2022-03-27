736f7000d0fc7c86eb2e9b4d2ac317904752c8d8
smlit30 <97806497+smlit30@users.noreply.github.com>
2022-03-27 10:01:00 -0500

    Add files via upload

--- PyCitySchools.py
+++ PyCitySchools.py
@@ -383,4 +383,47 @@ spending_summary_df["% Passing Reading"] = spending_summary_df["% Passing Readin
 
 spending_summary_df["% Overall Passing"] = spending_summary_df["% Overall Passing"].map("{:.0f}".format)
 
-print(spending_summary_df)
+#print(spending_summary_df)
+
+# Establish the bins.
+size_bins = [0, 999, 1999, 5000]
+group_names = ["Small (<1000)", "Medium (1000-1999)", "Large (2000-5000)"]
+
+# Categorize spending based on the bins.
+per_school_summary_df["School Size"] = pd.cut(per_school_summary_df["Total Students"], size_bins, labels=group_names)
+
+#print(per_school_summary_df)
+# Calculate averages for the desired columns.
+size_math_scores = per_school_summary_df.groupby(["School Size"]).mean()["Average Math Score"]
+
+size_reading_scores = per_school_summary_df.groupby(["School Size"]).mean()["Average Reading Score"]
+
+size_passing_math = per_school_summary_df.groupby(["School Size"]).mean()["% Passing Math"]
+
+size_passing_reading = per_school_summary_df.groupby(["School Size"]).mean()["% Passing Reading"]
+
+size_overall_passing = per_school_summary_df.groupby(["School Size"]).mean()["% Overall Passing"]
+
+# Assemble into DataFrame.
+size_summary_df = pd.DataFrame({
+          "Average Math Score" : size_math_scores,
+          "Average Reading Score": size_reading_scores,
+          "% Passing Math": size_passing_math,
+          "% Passing Reading": size_passing_reading,
+          "% Overall Passing": size_overall_passing})
+
+
+# Formatting.
+size_summary_df["Average Math Score"] = size_summary_df["Average Math Score"].map("{:.1f}".format)
+
+size_summary_df["Average Reading Score"] = size_summary_df["Average Reading Score"].map("{:.1f}".format)
+
+size_summary_df["% Passing Math"] = size_summary_df["% Passing Math"].map("{:.0f}".format)
+
+size_summary_df["% Passing Reading"] = size_summary_df["% Passing Reading"].map("{:.0f}".format)
+
+size_summary_df["% Overall Passing"] = size_summary_df["% Overall Passing"].map("{:.0f}".format)
+
+size_summary_df
+
+print(size_summary_df)
