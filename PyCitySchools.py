a6334c204508dfcd7c579c7a5e8b5a71d6078873
smlit30 <97806497+smlit30@users.noreply.github.com>
2022-03-27 10:03:43 -0500

    Add files via upload

--- PyCitySchools.py
+++ PyCitySchools.py
@@ -424,6 +424,36 @@ size_summary_df["% Passing Reading"] = size_summary_df["% Passing Reading"].map(
 
 size_summary_df["% Overall Passing"] = size_summary_df["% Overall Passing"].map("{:.0f}".format)
 
-size_summary_df
+#print(size_summary_df)
+# Calculate averages for the desired columns.
+type_math_scores = per_school_summary_df.groupby(["School Type"]).mean()["Average Math Score"]
 
-print(size_summary_df)
+type_reading_scores = per_school_summary_df.groupby(["School Type"]).mean()["Average Reading Score"]
+
+type_passing_math = per_school_summary_df.groupby(["School Type"]).mean()["% Passing Math"]
+
+type_passing_reading = per_school_summary_df.groupby(["School Type"]).mean()["% Passing Reading"]
+
+type_overall_passing = per_school_summary_df.groupby(["School Type"]).mean()["% Overall Passing"]
+
+# Assemble into DataFrame.
+type_summary_df = pd.DataFrame({
+          "Average Math Score" : type_math_scores,
+          "Average Reading Score": type_reading_scores,
+          "% Passing Math": type_passing_math,
+          "% Passing Reading": type_passing_reading,
+          "% Overall Passing": type_overall_passing})
+
+
+# Formatting
+type_summary_df["Average Math Score"] = type_summary_df["Average Math Score"].map("{:.1f}".format)
+
+type_summary_df["Average Reading Score"] = type_summary_df["Average Reading Score"].map("{:.1f}".format)
+
+type_summary_df["% Passing Math"] = type_summary_df["% Passing Math"].map("{:.0f}".format)
+
+type_summary_df["% Passing Reading"] = type_summary_df["% Passing Reading"].map("{:.0f}".format)
+
+type_summary_df["% Overall Passing"] = type_summary_df["% Overall Passing"].map("{:.0f}".format)
+
+print(type_summary_df)
